513cba1c9c84dc0272cbf69e56b26437ea37f31d
auhustsinovich <avgustinovich4@gmail.com>
2022-03-08 12:29:07 +0100

    Main Controller + constrains seated up

--- WelcomeUserApp/ViewController.swift
+++ WelcomeUserApp/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var userPassword: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func userNameTyped() {
+    }
+    
+    
+    
+    @IBAction func passwordTyped() {
+    }
+    
 
+    @IBAction func loginButtonPressed() {
+    }
+    
+    
+    
+    @IBAction func forgotNameButtonPressed() {
+    }
+    
+    @IBAction func forgotPasswordButtonPressed() {
+    }
+    
 }
 

0e9ccd0570a29ee1ebffad480364205781118043
auhustsinovich <avgustinovich4@gmail.com>
2022-03-09 17:56:00 +0100

    Touches began function fixed

--- WelcomeUserApp/LoginViewController.swift
+++ WelcomeUserApp/LoginViewController.swift
@@ -20,6 +20,7 @@ class LoginViewController: UIViewController {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super .touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
     
     @IBAction func userNameTyped() {
